70dfcf898eb96c90c2f61419ea9e508d2af776ca
skultew <113111982+skultew@users.noreply.github.com>
2022-12-25 22:17:00 +0300

    Update __init__.py

--- 4 lab/figures/__init__.py
+++ 4 lab/figures/__init__.py
@@ -1,3 +1,3 @@
-from .circle import circle_area, circle_perimeter 
+from .cricle import circle_area, circle_perimeter 
 from .square import square_perimeter, square_area 
 from .triangle import triangle_perimeter, triangle_area
