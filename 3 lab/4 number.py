44a27721e45b9e7252e1216b5e18169be3c1d6e3
skultew <113111982+skultew@users.noreply.github.com>
2022-12-25 20:54:18 +0300

    Update 4 number.py

--- 3 lab/4 number.py
+++ 3 lab/4 number.py
@@ -1,13 +1,9 @@
-def stairs(com):
-    for i in range(1, com + 1):
-        a = ''
-        for j in range(1, i + 1):
-            a = a + str(j)
-        for j in range(i - 1, 0, -1):
-            a = a + str(j)
-        print(a)
-    return 'End'
-com = 0
-com = int(input())
-
-print(stairs(com))
+n = int(input('Введите натуральное число: '))
+for i in range(1, n+1):
+    for a in range(1, n+1-i):
+        print(' ', end = '')
+    for b in range(1, i+1):
+        print(b, end = '')
+    for c in range(i-1, 0, -1):
+        print(c, end = '')
+    print()
